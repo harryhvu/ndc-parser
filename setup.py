29058869e1c2f9f780fd2340282a119bf7752f93
harryvu <harryvu1@live.com>
2022-06-22 21:15:47 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ndc-parser',
+    version='1.0',
+    packages=['ndcparser'],
+    url='https://github.com/harryhvu/ndc-parser',
+    license='MIT',
+    author='harryvu',
+    author_email='harryvu.py@gmail.com',
+    description='National drug code (NDC) normalizer and parser'
+)
